c0809d2f633e65795687654b69b27c75abaf24fd
Wandigos4 <dsevcuk49@gmail.com>
2026-06-18 05:22:13 +0300

    Add superuser creation hack

--- program/records/apps.py
+++ program/records/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
 
-
 class RecordsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'records'
+
+    def ready(self):
+        try:
+            from django.contrib.auth.models import User
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+                print("--- Адміністратор успішно створений! ---")
+        except:
+            pass
